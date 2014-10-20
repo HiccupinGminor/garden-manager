047e10fe5e904211de1c3137f7264f3a1d70cec5
Chris Chung <hiccupinGminor@gmail.com>
2014-10-19 22:56:32 -0700

    Add periodic wifi pinging

--- garden_manager.ino
+++ garden_manager.ino
@@ -73,9 +73,11 @@ int emergency_status_pin = 13;
 int moisture_reading;
 int moisture_reading_2;
 int n = 10;
-//int reading_delay = 30000;
-int reading_delay = 60000;
-int broadcast_delay = 3600000;
+
+long reading_delay = 60000;
+
+long broadcast_multiple = 30;
+int period_counter = 0;
 
 void setup(void)
 {
@@ -87,11 +89,11 @@ void setup(void)
   Serial.begin(115200);
   Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
   
-//  if (!cc3000.begin())
-//  {
-//    Serial.println(F("Couldn't begin()! Check your wiring?"));
-//    while(1);
-//  }
+  if (!cc3000.begin())
+  {
+    Serial.println(F("Couldn't begin()! Check your wiring?"));
+    while(1);
+  }
   
   // Optional SSID scan
   // listSSIDResults();
@@ -109,6 +111,13 @@ bool arrayCheckEqual(const int array[], int n)
 
 void loop(void)
 {
+  if(period_counter >= broadcast_multiple) {
+    //Reset
+    period_counter = 0;  
+  }
+  
+  period_counter++; 
+
   digitalWrite(ping_pin, HIGH);
   digitalWrite(ping_pin_2, HIGH);
   //Delay before taking measurements
@@ -122,11 +131,9 @@ void loop(void)
   for(int i = 0; i < n; i ++){
     if(i % 2 == 0) {
       readings[i] = analogRead(reading_pin);
-      Serial.println("Pin 1"); 
     }
     else {
       readings[i] = analogRead(reading_pin_2);
-      Serial.println("Pin 2");
     }
     
     delay(150);
@@ -156,77 +163,79 @@ void loop(void)
     digitalWrite(bad_status_pin, LOW); 
   }
   
-//  Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
-//  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
-//    Serial.println(F("Failed!"));
-//    while(1);
-//  }
-//   
-//  Serial.println(F("Connected!"));
-//  
-//  /* Wait for DHCP to complete */
-//  Serial.println(F("Request DHCP"));
-//  while (!cc3000.checkDHCP())
-//  {
-//    delay(100);
-//  }  
-
-//  ip = 0;
-  // Try looking up the website's IP address
-//  Serial.print(WEBSITE); Serial.print(F(" -> "));
-//  int resolve_attempts = 0;
-//  while (ip == 0) {
-//    if(resolve_attempts > 5) {
-//      return; 
-//    }
-//    if (! cc3000.getHostByName(WEBSITE, &ip)) {
-//      Serial.println(F("Couldn't resolve!"));
-//      resolve_attempts++;
-//    }
-//    delay(500);
-//  }
-
-  /* Try connecting to the website.
-     Note: HTTP/1.1 protocol is used to keep the server from closing the connection before all data is read.
-  */
- 
-//  char uri[77] = "/input/VGG3616pQEs9rG5VJj7r?private_key=9YYN1k1270h4zeK8oEPz&moisture=";
-//  char char_moisture_reading[20];
-//  
-//  strcat(uri, itoa(moisture_reading, char_moisture_reading, 10));
-//  
-//  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
-//  if (www.connected()) {
-//    www.fastrprint(F("GET "));
-//    www.fastrprint(uri);
-//    www.fastrprint(F(" HTTP/1.1\r\n"));
-//    www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n"));
-//    www.fastrprint(F("\r\n"));
-//    www.println();
-//  } else {
-//    Serial.println(F("Connection failed"));    
-//    return;
-//  }
-//
-//  Serial.println(F("-------------------------------------"));
+  if(period_counter == broadcast_multiple) {
+      
+      Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
+      if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+        Serial.println(F("Failed!"));
+        while(1);
+      }
+       
+      Serial.println(F("Connected!"));
+      
+      /* Wait for DHCP to complete */
+      Serial.println(F("Request DHCP"));
+      while (!cc3000.checkDHCP())
+      {
+        delay(100);
+      }  
+    
+      ip = 0;
+      // Try looking up the website's IP address
+      Serial.print(WEBSITE); Serial.print(F(" -> "));
+      int resolve_attempts = 0;
+      while (ip == 0) {
+        if(resolve_attempts > 5) {
+          return; 
+        }
+        if (! cc3000.getHostByName(WEBSITE, &ip)) {
+          Serial.println(F("Couldn't resolve!"));
+          resolve_attempts++;
+        }
+        delay(500);
+      }
+    
+      /* Try connecting to the website.
+         Note: HTTP/1.1 protocol is used to keep the server from closing the connection before all data is read.
+      */
+      char uri[77] = "/input/VGG3616pQEs9rG5VJj7r?private_key=9YYN1k1270h4zeK8oEPz&moisture=";
+      char char_moisture_reading[20];
+      
+      strcat(uri, itoa(moisture_reading, char_moisture_reading, 10));
+      
+      Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
+      if (www.connected()) {
+        www.fastrprint(F("GET "));
+        www.fastrprint(uri);
+        www.fastrprint(F(" HTTP/1.1\r\n"));
+        www.fastrprint(F("Host: ")); www.fastrprint(WEBSITE); www.fastrprint(F("\r\n"));
+        www.fastrprint(F("\r\n"));
+        www.println();
+      } else {
+        Serial.println(F("Connection failed"));    
+        return;
+      }
+    
+      Serial.println(F("-------------------------------------"));
+      
+      /* Read data until either the connection is closed, or the idle timeout is reached. */ 
+      unsigned long lastRead = millis();
+      while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+        while (www.available()) {
+          char c = www.read();
+          Serial.print(c);
+          lastRead = millis();
+        }
+      }
+      www.close();
+      Serial.println(F("-------------------------------------"));
+      
+      /* You need to make sure to clean up after yourself or the CC3000 can freak out */
+      /* the next time your try to connect ... */
+      Serial.println(F("\n\nDisconnecting"));
+      cc3000.disconnect();
+  }
   
-  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
-//  unsigned long lastRead = millis();
-//  while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
-//    while (www.available()) {
-//      char c = www.read();
-//      Serial.print(c);
-//      lastRead = millis();
-//    }
-//  }
-//  www.close();
-//  Serial.println(F("-------------------------------------"));
-//  
-  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
-  /* the next time your try to connect ... */
-//  Serial.println(F("\n\nDisconnecting"));
-//  cc3000.disconnect();
-//  
   delay(reading_delay);
 }
 
